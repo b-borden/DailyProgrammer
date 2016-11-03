a83dffb64c29cb2eddaab9c92e6613503af24b9d
Brandon <bborden@ualberta.ca>
2016-11-02 21:33:49 -0700

    Adding 290e

---
+++ 290e/290e.py
@@ -0,0 +1,10 @@
+def kaprekar(start, stop):
+    for n in range(start, stop+1):
+        sqd = str(n**2)
+        for i in range(1, len(sqd)):
+            l = int(sqd[:i])
+            r = int(sqd[i:])
+            if r != 0 and l + r == n:
+                yield n
+
+print(list(kaprekar(1, 9000)))
