e8d740d98ba29a2f1cd9a45574bc2b848b721b3e
Brandon <bborden@ualberta.ca>
2016-11-02 23:53:12 -0700

    FInished, including bonus 1

--- 289e/289e.py
+++ 289e/289e.py
@@ -1,5 +1,10 @@
+# Including bonus 1
+
 import re
 import requests
+import operator
+from functools import reduce
+
 from httpcache import CachingHTTPAdapter
 
 # Let's be nice to pokeapi and cache our requests
@@ -7,11 +12,26 @@ s = requests.Session()
 s.mount('http://', CachingHTTPAdapter())
 
 def dmg_mult(attack):
-	move_type, def_type = re.split('\s*->\s*', attack)
+	atk_types, def_types = (types.split() for types in re.split('\s*->\s*', attack))
 
-	attack_dmg_info = s.get('http://pokeapi.co/api/v2/type/' + move_type + '/').json()['damage_relations']
-	def_dmg_info = s.get('http://pokeapi.co/api/v2/type/' + def_type + '/').json()['damage_relations']
-	return attack_dmg_info, def_dmg_info
+	dmg = [1.0]
+	for attack_type in atk_types:
+		atk_info = s.get('http://pokeapi.co/api/v2/type/' + attack_type + '/').json()['damage_relations']
+		atk_info = {effect_desc: [_type['name'] for _type in types] for effect_desc, types in atk_info.copy().items()}
+
+		dmg.extend([0.0 for def_type in def_types if def_type in atk_info['no_damage_to']])
+		dmg.extend([0.5 for def_type in def_types if def_type in atk_info['half_damage_to']])
+		dmg.extend([2.0 for def_type in def_types if def_type in atk_info['double_damage_to']])
+
+	return reduce(operator.mul, dmg)
 
 
-print(dmg_mult('grass -> grass'))
+atks = '''fire -> grass
+ fighting -> ice rock
+ psychic -> poison dark
+ water -> normal
+ fire -> rock'''
+
+for atk in atks.splitlines():
+	print('%.gx' % dmg_mult(atk))
+
