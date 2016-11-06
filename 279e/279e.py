cdc633fa93777ff8c7c7ceda02e38a910d90681b
Brandon <bborden@ualberta.ca>
2016-11-06 15:33:06 -0800

    Codecs library has a uu encoding that handles the begin and end delimiters  (in a standard way?) It also has no 45 byte max limitation, simplifying code.  The decode side is symmetric, and consumes the delimeters. How Pythonic! Bonus complete

--- 279e/279e.py
+++ 279e/279e.py
@@ -1,33 +1,23 @@
-# Turns out that python has a built-in to do uuencoding. Just need to convert input into bytes,
-# pad zeros at end if necessary to make bytes length a multiple of 3.  Lastly, chunk input into 45 byte groups
-# to workaround a 45-byte max limitation of the built-in uuencode limitation
+# Turns out that python has a built-in to do uuencoding. Just need to convert input into bytes
+# and pad zeros at end if necessary to make bytes length a multiple of 3.
 
-import binascii
-
-def chunk(e, _size):
-	for i in range(0, len(e), _size):
-		yield e[i:i+_size]
+import codecs
 
 def uuencode(msg):
-	bytes = str.encode(msg)
+	bytes = msg.encode()
 	# Pad to an even multiple of 3 bytes with zeros
 	bytes += b'0' * ((3 - (len(bytes) % 3)) % 3)
-	uuencoded = ''.join([binascii.b2a_uu(byte_chunk).decode() for byte_chunk in chunk(bytes, 45)])
+	uuencoded = codecs.encode(bytes, 'uu')
 
-	return uuencoded.strip()
+	return uuencoded.decode()
 
 def uudecode(msg):
-	for line in msg.splitlines():
-		pass
+	return codecs.decode(msg.encode(), 'uu').decode()
 
 uu1 = uuencode('Cat')
 uu2 = uuencode('''I feel very strongly about you doing duty. Would you give me a little more documentation about your reading in French? I am glad you are happy — but I never believe much in happiness. I never believe in misery either. Those are things you see on the stage or the screen or the printed pages, they never really happen to you in life.''')
-print('begin 644 cat.txt')
 print(uu1)
-print('`')
-print('begin 644 text.txt')
 print(uu2)
-print('`')
 
 print('Bonus 1:')
 print(uudecode(uu1))
