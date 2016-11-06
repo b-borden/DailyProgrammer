a57d6239b2c8e12e46ed0d008f6d8c630d2f00d7
Brandon <bborden@ualberta.ca>
2016-11-06 01:11:24 -0800

    Missed condition that it can't be an anagram if the phrases can't be word shuffled versions of eachother

--- 283e/283e.py
+++ 283e/283e.py
@@ -1,8 +1,8 @@
 import re
 
 def find_anagram(inputs):
-	l,r = inputs.lower().split('?')
-	if sorted(re.sub(r'\W', '', l)) == sorted(re.sub(r'\W', '', r)):
+	l,r = inputs.replace('"', '').lower().split('?')
+	if sorted(re.sub(r'\W', '', l)) == sorted(re.sub(r'\W', '', r)) and sorted(l.split()) != sorted(r.split()):
 		return re.sub('\?', 'is an anagram of', inputs)
 	else:
 		return re.sub('\?', 'is NOT an anagram of', inputs)
@@ -13,7 +13,8 @@ inputs = '''"wisdom" ? "mid sow"
 "Schoolmaster" ? "The classroom"
 "Astronomers" ? "Moon starer"
 "Vacation Times" ? "I'm Not as Active"
-"Dormitory" ? "Dirty Rooms"'''
+"Dormitory" ? "Dirty Rooms"
+"Just shuffled phrase" ? "Phrase just shuffled"'''
 
 for line in inputs.splitlines():
 	print(find_anagram(line))
